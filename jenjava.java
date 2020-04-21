abae4f58dbe8b8be1277d9f8247092d8acb361a6
Aravinda Kumar <Varun Kumar@DESKTOP-C7TUAL6.sc.com>
2020-04-21 17:47:56 +0530

    good even

--- jenjava.java
+++ jenjava.java
@@ -2,6 +2,7 @@ class jenjava{
     public static void main(String args[]){  
      System.out.println("new project started");
      System.out.println("new project team welcome");
+     System.out.println("good evening");
     }  
 }
  
