5c7e275e9866028c0a304dc84ca9174ce320854b
Aravinda Kumar <Varun Kumar@DESKTOP-C7TUAL6.sc.com>
2020-04-21 17:42:18 +0530

    welcome new team

--- jenjava.java
+++ jenjava.java
@@ -1,6 +1,7 @@
 class jenjava{  
     public static void main(String args[]){  
-     System.out.println("new project started");  
+     System.out.println("new project started");
+     System.out.println("new project team welcome");
     }  
 }
  
